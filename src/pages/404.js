8a50dda922c90015ddd123c6806055f600d42d73
minhnh <minhnh.da@gmail.com>
2019-09-09 00:12:28 +0900

    [fix] Fix 404 page layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,32 +1,31 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Layout from "../components/layout"
+import Language from "../lang/languages"
 
 class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+	render() {
+		const { location } = this.props
+		let language = "en"
+		if (location.pathname.includes("/vi/")) {
+			language = "vi"
+		} else if (location.pathname.includes("/ja/")) {
+			language = "ja"
+		}
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
+		const homeLink = Language[language]["navLinks"]["home"]
+
+		return (
+			<Layout location={location} language={language}>
+				<SEO title="404: Not Found"/>
+				<h1>Not Found</h1>
+				<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+				<Link to={homeLink}>Back to home.</Link>
+			</Layout>
+		)
+	}
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
